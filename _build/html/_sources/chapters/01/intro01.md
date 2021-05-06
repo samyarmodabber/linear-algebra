7e23d20fa9e7a8349fd544dfc71174ffd5bfc3cf
Samyar  Modabber <samyar.modabber@gmail.com>
2021-05-06 18:12:31 +0200

    push change

---
+++ _build/html/_sources/chapters/01/intro01.md
@@ -0,0 +1,8 @@
+# Chapter 1: Fundamental of Vctor Space
+
+We introduce fundamental definitions and Theorem in vector spaces(VS)
+
+
+
+Refrences:
+[Wikipedia]: http://wikipedia.com
---
+++ _build/jupyter_execute/chapters/01/01-vector-space.py
@@ -0,0 +1,25 @@
+# Vector Space
+
+## Definitions
+**Vector Space**
+
+A vector space over a field $F$ (slalers) is a set $V$ (vectors) together with two operations, addition (vector addition)$+:V\times V\to V$ and multiplication (scalar multiplication) $\cdot :F\times V\to V$ such that:
+1. $\left( V,+ \right)$ is an abelian group.
+2. Compatibility:
+
+$$\forall a,\ b\ \in F\ \forall v\in V\ \ \ \ a\left( bv \right)=\left( ab \right)v$$
+3. Identity:
+
+$$\ \forall v\in V\ \ \ \ 1v=v\,\,\,\,\,st\ \ 1={{1}_{F}}\ \in F$$
+4. Distributivity:
+
+$$\forall a,\ b\ \in F\ \forall u,v\in V\ \ \ \ a\left( u+v \right)=au+av\ \ \ \wedge \ \ \ \left( a+b \right)v=av+bv$$
+
+## Examples
+1. The real numbers $\mathbb{R}$, with the usual addition and multiplication form a field. 
+2. The Rational numbers $\mathbb{Q}$, with the usual addition and multiplication form a field.
+3. The complex numbers $\mathbb{C}$ with the usual addition and multiplication form a field. 
+
+Recall that for ${{z}_{1}}={{a}_{1}}+{{b}_{1}}i,\ {{z}_{2}}={{a}_{2}}+{{b}_{2}}i\ \in \ \mathbb{C}\ $ define multiplication as:
+
+$${{z}_{1}}{{z}_{2}}=\left( {{a}_{1}}{{a}_{2}}-{{b}_{1}}{{b}_{2}} \right)+\left( {{a}_{1}}{{b}_{2}}+{{a}_{2}}{{b}_{1}} \right)i\ \in \ \mathbb{C}\ $$
